b33363bf5832ce0b37951ca4c16abf736789d3d7
kevinbednarek <kevinbednarek@gmail.com>
2024-05-21 16:23:52 -0500

    Added rat and trap drawings to game board

--- src/ui.rs
+++ src/ui.rs
@@ -1,18 +1,13 @@
-use ratatui::{
-    layout::Alignment,
-    style::{Color, Style, Stylize},
-    widgets::{canvas::*, *},
-    Frame,
-};
+use ratatui::{layout::Alignment, style::{Color, Style, Stylize}, widgets::{*}, Frame, text};
 use ratatui::layout::{Constraint, Direction, Layout, Rect};
-use ratatui::prelude::{Buffer, Marker};
-use ratatui::text::{Line, Span, Text};
+use ratatui::text::{Span, Text};
 use crate::app::App;
 
 pub fn render(app: &mut App, frame: &mut Frame) {
     let sections = Layout::default()
         .direction(Direction::Vertical)
         .constraints([
+            Constraint::Length(5),
             Constraint::Min(1),
             Constraint::Length(3),
             Constraint::Length(3),
@@ -20,93 +15,61 @@ pub fn render(app: &mut App, frame: &mut Frame) {
         ])
         .split(frame.size());
 
-    frame.render_widget(
-        Paragraph::new(format!("`Enter` for hint | `Esc` to quit\n\
-                Guess incorrectly 6 times and you lose!\n\
-                Incorrect guesses: {}", app.strikes)
-        )
-        .block(
-            Block::bordered()
-                .title(" Rat Trap ")
-                .title_alignment(Alignment::Center)
-                .border_type(BorderType::Rounded),
-        )
-        .style(Style::default().fg(Color::Cyan).bg(Color::Black))
-        .centered(),
-        frame.size(),
+    let game_area = Layout::horizontal([
+        Constraint::Min(1),
+        Constraint::Min(1),
+        Constraint::Min(1),
+        Constraint::Min(1),
+        Constraint::Min(1),
+        Constraint::Min(1),
+        Constraint::Min(1),
+        Constraint::Min(1),
+    ])
+        .split(sections[1]);
+
+    //Render widget for whole background. Make it black, give everything a border
+    frame.render_widget(Block::bordered()
+        .title(" Rat Trap ")
+        .title_alignment(Alignment::Center)
+        .border_type(BorderType::Rounded)
+        .style(Style::default()
+            .fg(Color::Cyan)
+            .bg(Color::Black)), frame.size(),
     );
 
-    /*--- Hyphens displayed for the letters of the word to guess ---*/
-    let mut displayed_game = Vec::new();
-    for letter in app.game.iter() {
-        displayed_game.push(Span::from(letter.clone()));
-    };
+    //Render game instructions
+    render_game_info(app.strikes, sections[0], frame);
 
-    let letters = Text::from(Line::from(displayed_game));
+    //Render game letters
+    render_letters(&app.game, sections[3], frame);
 
-    frame.render_widget(Paragraph::new(letters)
-                            .centered()
-                            .block(Block::bordered()
-                                .border_type(BorderType::Rounded)), sections[2]);
+    //Render guesses
+    render_letters(&app.guesses, sections[4], frame);
 
-
-    /*--- Alphabet displayed for guesses ---*/
-    let mut displayed_guesses = Vec::new();
-    for guess in app.guesses.iter() {
-        displayed_guesses.push(Span::from(guess.clone().red().bold()));
-    };
-
-    let guesses = Text::from(Line::from(displayed_guesses));
-
-    frame.render_widget(Paragraph::new(guesses)
-                            .centered()
-                            .block(Block::bordered()
-                                .border_type(BorderType::Rounded)), sections[3]);
-
-    /*--- Conditionally display a hint ---*/
+    //Conditionally render hint
     if app.hint_display {
-        frame.render_widget(Paragraph::new("Hint: ".to_owned() + &*app.hint)
-                                .centered()
-                                .block(Block::bordered()
-                                    .border_type(BorderType::Rounded)), sections[1]);
+        render_hint(&*app.hint, sections[2], frame);
     }
 
-    /*--- Popup for the game result ---*/
-    match app.game_result {
-        Some(true) => {
-            let area = centered_rect(60, 30, frame.size());
-            //frame.render_widget(Clear, area); //this clears out the background
-            frame.render_widget(Paragraph::new(
-                "You Win!\n\
-                Press `Esc` to quit")  //TODO: Implement "play again" feature
-                                    .centered()
-                                    .block(Block::bordered()
-                                        .border_type(BorderType::Rounded)), area);
-        }
-        Some(false) => {
-            let area = centered_rect(60, 30, frame.size());
-            //frame.render_widget(Clear, area); //this clears out the background
-            frame.render_widget(Paragraph::new(format!(
-                "You Lose!\n\
-                The word was '{}'\n\
-                Press `Esc` to quit", app.word))  //TODO: Implement "play again" feature
-                                    .centered()
-                                    .block(Block::bordered()
-                                        .border_type(BorderType::Rounded)), area);
-        }
-        _ => {}
-    }
+
+
+    draw_trap(game_area[6], frame);
+    //TODO: Calculate which rat to use based on how much space (height) there is to work with
+    let rat_space = sections[1].height;
 
     match app.strikes {
-        1 => {/*TODO: Draw something*/}
-        2 => {/*TODO: Draw something*/}
-        3 => {/*TODO: Draw something*/}
-        4 => {/*TODO: Draw something*/}
-        5 => {/*TODO: Draw something*/}
-        6 => {/*TODO: Draw last thing*/}
-        _ => {}
-
+        0 => draw_rat(game_area[0], frame),
+        1 => draw_rat(game_area[1], frame),
+        2 => draw_rat(game_area[2], frame),
+        3 => draw_rat(game_area[3], frame),
+        4 => draw_rat(game_area[4], frame),
+        5 => draw_rat(game_area[5], frame),
+        _ => draw_rat(game_area[5], frame),
     }
+
+    //Popup for the game result
+    //This need to render last to cover everything in the background
+    render_game_result(app.game_result, &app.word, frame);
 }
 
 //Helper function for popup
@@ -126,92 +89,111 @@ fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
         .split(popup_layout[1])[1]
 }
 
-
-/*fn draw_something(x: f64, y: f64) -> impl Widget + '_ {
-    Canvas::default()
-        .block(Block::default().borders(Borders::ALL).title("World"))
-        .marker(self.marker)
-        .paint(|ctx| {
-            ctx.draw(&Map {
-                color: Color::Green,
-                resolution: MapResolution::High,
-            });
-            ctx.print(self.x, -self.y, "You are here".yellow());
-        })
-        .x_bounds([-180.0, 180.0])
-        .y_bounds([-90.0, 90.0])
-}*/
-
-fn draw_map()  {
-    let mut buffer = Buffer::empty(Rect::new(0, 0, 80, 40));
-    let canvas = Canvas::default()
-        .marker(Marker::Braille)
-        .x_bounds([-180.0, 180.0])
-        .y_bounds([-90.0, 90.0])
-        .paint(|context| {
-            context.draw(&Map {
-                resolution: MapResolution::High,
-                ..Default::default()
-            });
-        });
-    canvas.render(buffer.area, &mut buffer);
+/*------------------------------ Frame Rendering Functions ------------------------------*/
+fn render_game_info(strikes: u8, r: Rect, frame: &mut Frame) {
+    frame.render_widget(
+        Paragraph::new(format!("`Enter` for hint | `Esc` to quit\n\
+                Guess incorrectly 6 times and you lose!\n\
+                Incorrect guesses: {}", strikes)
+        )
+            .block(
+                Block::bordered()
+                    .title(" Rat Trap ")
+                    .title_alignment(Alignment::Center)
+                    .border_type(BorderType::Rounded),
+            )
+            .style(Style::default().fg(Color::Cyan).bg(Color::Black))
+            .centered(),
+        r,
+    );
 }
 
+fn render_letters(letters: &Vec<String>, r: Rect, frame: &mut Frame) {
+    let mut displayed_guesses = Vec::new();
+    for letter in letters.iter() {
+        displayed_guesses.push(Span::from(letter.clone().red().bold()));
+    };
 
-fn draw_high() {
-    let mut buffer = Buffer::empty(Rect::new(0, 0, 80, 40));
-    let canvas = Canvas::default()
-        .marker(Marker::Braille)
-        .x_bounds([-180.0, 180.0])
-        .y_bounds([-90.0, 90.0])
-        .paint(|context| {
-            context.draw(&Map {
-                resolution: MapResolution::High,
-                ..Default::default()
-            });
-        });
-    canvas.render(buffer.area, &mut buffer);
-    let expected = Buffer::with_lines([
-        "                                                                                ",
-        "                  ⢀⣠⠤⠤⠤⠔⢤⣤⡄⠤⡠⣄⠢⠂⢢⠰⣠⡄⣀⡀                      ⣀                   ",
-        "            ⢀⣀⡤⣦⠲⢶⣿⣮⣿⡉⣰⢶⢏⡂        ⢀⣟⠁     ⢺⣻⢿⠏   ⠈⠉⠁ ⢀⣀    ⠈⠓⢳⣢⣂⡀               ",
-        "            ⡞⣳⣿⣻⡧⣷⣿⣿⢿⢿⣧⡀⠉⠉⠙⢆      ⣰⠇               ⣠⠞⠃⢉⣄⣀⣠⠴⠊⠉⠁ ⠐⠾⠤⢤⠤⡄⠐⣻⠜⢓⠂      ",
-        "⢍ ⢀⡴⠊⠙⠓⠒⠒⠤⠖⠺⠿⠽⣷⣬⢬⣾⣷⢻⣷⢲⢲⣍⠱⡀ ⠹⡗   ⢀⢐⠟        ⡔⠒⠉⠲⠤⢀⢄⡀⢩⣣⠦⢷⢼⡏⠈          ⠉⠉⠉ ⠈⠈⠉⠖⠤⠆⠒⠭",
-        "⠶⢽⡲⣽⡆             ⠈⣠⣽⣯⡼⢯⣘⡯⠃⠘⡆ ⢰⠒⠁ ⢾⣚⠟    ⢀⠆ ⣔⠆ ⢷⠾⠋⠁    ⠙⠁                     ⠠⡤",
-        "  ⠠⢧⣄⣀⡶⠦⠤⡀        ⢰⡁ ⠉⡻⠙⣎⡥  ⠘⠲⠇       ⢀⡀⠨⣁⡄⣸⢫⡤⠄                        ⣀⢠⣤⠊⣼⠅⠖⠋⠁",
-        "   ⣠⠾⠛⠁  ⠈⣱        ⠋⠦⢤⡼ ⠈⠈⠦⡀         ⢀⣿⣇ ⢹⣷⣂⡞⠃                       ⢀⣂⡀  ⠏⣜    ",
-        "          ⠙⣷⡄        ⠘⠆ ⢀⣀⡠⣗         ⠘⣻⣽⡟⠉⠈                           ⢹⡇  ⠟⠁    ",
-        "           ⠈⡟           ⢎⣻⡿⠾⠇         ⠘⠇  ⣀⡀  ⣤⣤⡆ ⡠⡦                 ⢀⠎⡏        ",
-        "            ⡇          ⣀⠏⠋           ⢸⠒⢃⡖⢻⢟⣷⣄⣰⣡⠥⣱ ⢏⣧              ⣀ ⡴⠚⢰⠟        ",
-        "            ⢳         ⢸⠃             ⠸⣄⣼⣠⢼⡴⡟⢿⢿⣀⣄  ⠸⡹             ⠘⡯⢿⡇⡠⢼⠁        ",
-        "             ⢳⣀      ⢀⠞⠁             ⢠⠋⠁ ⠐⠧⡄⣬⣉⣈⡽                  ⢧⠘⢽⠟⠉         ",
-        "              ⣿⣄  ⡴⠚⠛⣿⣀             ⢠⠖     ⠈⠁ ⠹⣧  ⢾⣄⡀             ⡼ ⠈           ",
-        "    ⣀         ⠘⣿⡄ ⡇  ⣘⣻             ⡏          ⢻⡄ ⠘⠿⢿⠒⠲⡀   ⢀⡀   ⢀⡰⣗             ",
-        "    ⠉⠷          ⢫⡀⢧⡼⡟⠉⣛⣳⣦⡀         ⠈⡇          ⠸⣱  ⢀⡼  ⢺  ⡸⠉⢇  ⣾⡏ ⣁             ",
-        "                 ⠉⠒⢆⡓⡆             ⠠⡃           ⢳⣇⡠⠏   ⠐⡄⡞  ⠘⣇⡀⢱  ⣾⡀            ",
-        "                    ⢹⣇⣀⣾⡷⠤⡆         ⢣            ⠯⢺⠇    ⢣⣅   ⣽⢱⡔ ⢠⢿⣗            ",
-        "                     ⠙⢱   ⠘⠦⡄       ⠈⢦⡠⣠⢶⣀        ⡜     ⠈⠿  ⢠⣽⢆ ⢀⣼⡜⠿            ",
-        "                     ⢀⡞     ⢱⡀           ⢸       ⡔⠁          ⢻⢿⢰⠏⢸⣤⣴⣆           ",
-        "                     ⢘⠆      ⠙⠢⢄         ⠸⡀     ⡸⠁           ⠈⣞⡎⠥⡟⣿⠠⠿⣷⠒⢤⢀⣆      ",
-        "                     ⠘⠆        ⢈⠂         ⢳     ⡇             ⠈⠳⠶⣤⣭⣠ ⠋⢧⡬⣟⠉⠷⡄    ",
-        "                      ⢨        ⡜          ⢸     ⠸ ⣠               ⠁⢁⣰⢶ ⡇⠉⠁ ⠛    ",
-        "⠆                     ⠈⢱⡀      ⡆          ⡇    ⢀⡜⡴⢹               ⢰⠏⠁⠘⢶⠹⡀   ⠸ ⢠⡶",
-        "                        ⠅     ⣸           ⢸    ⢫ ⡞⡊             ⢠⠔⠋     ⢳⡀ ⠐⣦   ",
-        "                        ⡅    ⡏            ⠈⡆  ⢠⠎ ⠳⠃             ⢸        ⢳      ",
-        "                       ⠨    ⡸⠁             ⢱  ⡸                 ⠈⡇ ⢀⣀⡀   ⢸      ",
-        "                       ⠸  ⠐⡶⠁              ⠘⠖⠚                   ⠣⠒⠋ ⠱⣇ ⢀⠇   ⠰⡄ ",
-        "                       ⠽ ⣰⡖⠁                                          ⠘⢚⡊    ⢀⣿⠇",
-        "                       ⡯⢀⡟                                             ⠘⠏   ⢠⢾⠃ ",
-        "                       ⠇⢨⠆                            ⢠⡄                    ⠈⠁  ",
-        "                       ⢧⣷⡀⠚                                                     ",
-        "                        ⠉⠁                                                      ",
-        "                          ⢀⡀                                                    ",
-        "                        ⢠⡾⠋                      ⣀⡠⠖⢦⣀⣀  ⣀⠤⠦⢤⠤⠶⠤⠖⠦⠤⠤⠤⠴⠤⢤⣄       ",
-        "                ⢀⣤⣀ ⡀  ⣼⣻⠙⡆         ⢀⡤⠤⠤⠴⠒⠖⠒⠒⠒⠚⠉⠋⠁    ⢰⡳⠊⠁              ⠈⠉⠉⠒⠤⣤  ",
-        "    ⢀⣀⣀⡴⠖⠒⠒⠚⠛⠛⠛⠒⠚⠳⠉⠉⠉⠉⢉⣉⡥⠔⠃     ⢀⣠⠤⠴⠃                                      ⢠⠞⠁  ",
-        "   ⠘⠛⣓⣒⠆              ⠸⠥⣀⣤⡦⠠⣞⣭⣇⣘⠿⠆                                         ⣖⠛   ",
-        "⠶⠔⠲⠤⠠⠜⢗⠤⠄                 ⠘⠉  ⠁                                            ⠈⠉⠒⠔⠤",
-        "                                                                                ",
-    ]);
-    assert_eq!(buffer, expected);
+    let guesses = Text::from(text::Line::from(displayed_guesses));
+
+    frame.render_widget(Paragraph::new(guesses)
+                            .centered()
+                            .block(Block::bordered()
+                                .border_type(BorderType::Rounded))
+                            .style(Style::default().fg(Color::Cyan).bg(Color::Black))
+                            .centered(), r);
+}
+
+fn render_hint(hint: &str, r: Rect, frame: &mut Frame) {
+    frame.render_widget(Paragraph::new("Hint: ".to_owned() + hint)
+                            .centered()
+                            .block(Block::bordered()
+                                .border_type(BorderType::Rounded))
+                            .style(Style::default().fg(Color::Cyan).bg(Color::Black))
+                            .centered(), r);
+}
+
+fn render_game_result(game_result: Option<bool>, word: &String, frame: &mut Frame) {
+    //TODO: Use this for win/lose text --> https://crates.io/crates/tui-big-text
+    //TODO: Implement "play again" feature
+    match game_result {
+        Some(true) => {
+            let area = centered_rect(60, 30, frame.size());
+            frame.render_widget(Clear, area); // clears out the background
+            frame.render_widget(Paragraph::new(
+                "You Win!\n\
+                Press `Esc` to quit")
+                                    .centered()
+                                    .block(Block::bordered()
+                                        .border_type(BorderType::Rounded)
+                                        .style(Style::default()
+                                            .fg(Color::Cyan)
+                                            .bg(Color::Black))), area);
+        }
+        Some(false) => {
+            let area = centered_rect(60, 30, frame.size());
+            frame.render_widget(Clear, area); // clears out the background
+            frame.render_widget(Paragraph::new(format!(
+                "You Lose!\n\
+                The word was '{}'\n\
+                Press `Esc` to quit", word))
+                                    .centered()
+                                    .block(Block::bordered()
+                                        .border_type(BorderType::Rounded)
+                                        .style(Style::default()
+                                            .fg(Color::Cyan)
+                                            .bg(Color::Black))), area);
+        }
+        _ => {}
+    }
+}
+
+fn draw_rat(r: Rect, frame: &mut Frame) {
+    let rat = vec![
+        "      _____()()" .into(),
+        "     /       @@" .into(),
+        "~~~~~|_;m__m._>o".into(),
+    ];
+
+    frame.render_widget(Paragraph::new(rat)
+                            .centered()
+                            .style(Style::default().fg(Color::Cyan).bg(Color::Black))
+                            .centered(), r);
+}
+
+fn draw_trap(r: Rect, frame: &mut Frame) {
+    let trap = vec![
+        "     .----------.-----------.".into(),
+        "    / ____    ;..;=====.   //".into(),
+        "   / /|o  |  ((()    //   // ".into(),
+        "  / /o|  o| ((()    //   //  ".into(),
+        " / /o_|_o_|((()====='   //   ".into(),
+        "/___________'__________//    ".into(),
+        "`----------'----------'`     ".into(),
+    ];
+
+    frame.render_widget(Paragraph::new(trap)
+                            .centered()
+                            .style(Style::default().fg(Color::Cyan).bg(Color::Black))
+                            .centered(), r);
 }
